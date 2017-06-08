5378fb7a4a75bee809bf520b1a14f3415306b3df
Angela Liu <liuangela2001@gmail.com>
2017-06-08 18:52:36 +0000

    Add styling to personal website

---
+++ main.js
@@ -0,0 +1,7 @@
+function popUp() {
+
+  var popMessage= "Hope these chow chows brightened your day! If you're interested pets, check out https://www.petfinder.com"
+  alert(popMessage);
+}
+
+$("#button").on("click", popUp);
